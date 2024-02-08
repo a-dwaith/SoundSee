a549b5ceec7a1d0624c66e2e8ac2c05dfe15091a
Adwaith <adhu.adwaith.m.k@gmail.com>
2024-02-08 22:35:29 +0530

    Added backend to login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,143 +1,210 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:soundsee/pages/componets/button.dart';
 import 'package:soundsee/pages/componets/squared_tile.dart';
 import 'pages/componets/text_field.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-class LogInPage extends StatelessWidget {
-  LogInPage({super.key});
+class LogInPage extends StatefulWidget {
+  const LogInPage({super.key});
 
-  final usernameController = TextEditingController();
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   // Sign user
+  void signUserIn() async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+      Navigator.pop(context);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+      if (e.code == 'user-not-found') {
+        wrongUsermessage();
+      } else if (e.code == 'wrong-password') {
+        wrongPasswordmessage();
+      }
+    }
+  }
 
-  void signUserIn() {}
+// todo Wrong username error message
+  void wrongUsermessage() {
+    const snackBar = SnackBar(
+      content: Text(
+        'Wrong username! Try again',
+        style: TextStyle(color: Colors.white),
+      ),
+      duration: Duration(seconds: 3),
+      backgroundColor: Colors.black,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  //todo Wrong password message
+  void wrongPasswordmessage() {
+    const snackBar = SnackBar(
+      content: Text(
+        'Wrong password! Try again',
+        style: TextStyle(color: Colors.white),
+      ),
+      duration: Duration(seconds: 3),
+      backgroundColor: Colors.black,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // Just a Lock Logo
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // Just a Lock Logo
 
-                children: [
-                  const SizedBox(height: 50),
-                  Icon(
-                    Icons.lock,
-                    size: 100,
-                    // color: Colors.blue,
-                    color: HexColor("#0066FF"),
-                  ),
+              children: [
+                const SizedBox(height: 50),
+                Icon(
+                  Icons.lock,
+                  size: 100,
+                  // color: Colors.blue,
+                  color: HexColor("#0066FF"),
+                ),
 
-                  const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-                  // Welcome note
+                // Welcome note
 
-                  const Text(
-                    'Welcome back you\'ve been missed',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold),
-                  ),
+                const Text(
+                  'Welcome back you\'ve been missed',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
 
-                  const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-                  // Username text box
+                // Username text box
 
-                  // Text field
-                  MyTextfield(
-                    controller: usernameController,
-                    hintText: "Username",
-                    obscureText: false,
-                  ),
+                // Text field
+                MyTextfield(
+                  controller: emailController,
+                  hintText: "Email",
+                  obscureText: false,
+                ),
 
-                  const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-                  // Password Text box
+                // Password Text box
 
-                  MyTextfield(
-                    controller: passwordController,
-                    hintText: "Password",
-                    obscureText: true,
-                  ),
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
 
-                  const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Navigate to the forgot password page
+                          Navigator.pushNamed(context, '/forgot_password');
+                        },
+                        child: Text(
                           "Forgot password?",
                           style: TextStyle(color: HexColor("#0066FF")),
                         ),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(height: 25),
-
-                  MyButton(
-                    onTap: signUserIn,
-                  ),
-
-                  const SizedBox(height: 50),
-
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Text("Or continue with"),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SquaredTile(imagePath: 'lib/assets/images/Google.png')
+                      ),
                     ],
                   ),
-                  const SizedBox(height: 50),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                ),
+
+                const SizedBox(height: 25),
+
+                MyButton(
+                  onTap: signUserIn,
+                ),
+
+                const SizedBox(height: 50),
+
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
                     children: [
-                      const Text("Not a member?"),
-                      const SizedBox(
-                        width: 4,
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                        ),
                       ),
-                      Text(
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text("Or continue with"),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+                const Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SquaredTile(imagePath: 'lib/assets/images/Google.png')
+                  ],
+                ),
+                const SizedBox(height: 50),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Not a member?"),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // Navigate to the forgot password page
+                        Navigator.pushNamed(context, '/signUp');
+                      },
+                      child: Text(
                         "Register now!",
                         style: TextStyle(
                           color: HexColor("#0066FF"),
                         ),
                       ),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
